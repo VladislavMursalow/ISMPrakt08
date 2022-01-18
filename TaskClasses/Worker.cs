2d69369bce1243f134689faa5301f9968e36eefe
Mursalow Vlad <ki201_mvr@student.ztu.edu.ua>
2022-01-18 14:40:25 +0200

    Add constructors to Worker class

--- TaskClasses/Worker.cs
+++ TaskClasses/Worker.cs
@@ -15,5 +15,33 @@ namespace TaskClasses
         public WorkPlace WorkPlace { get; set; }
         public int Salary { get; set; }
         public int Prize { get; set; }
+
+        public Worker()
+        {
+
+        }
+
+        public Worker(string firstname, string lastname, string surname, string data, WorkPlace workPlace, int salary, int prize)
+        {
+            Firstname = firstname;
+            Lastname = lastname;
+            Surname = surname;
+            Data = data;
+            WorkPlace = workPlace;
+            Salary = salary;
+            Prize = prize;
+
+        }
+
+        public Worker(Worker a)
+        {
+            Firstname = a.Firstname;
+            Lastname = a.Lastname;
+            Surname = a.Surname;
+            Data = a.Data;
+            WorkPlace = a.WorkPlace;
+            Salary = a.Salary;
+            Prize = a.Prize;
+        }
     }
 }
