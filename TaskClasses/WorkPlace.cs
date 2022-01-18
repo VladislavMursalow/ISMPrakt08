c28f3ca3a171c898ca67b84e7648a8665ee15bca
Mursalow Vlad <ki201_mvr@student.ztu.edu.ua>
2022-01-18 14:24:40 +0200

    Added constuctors to WorkPlace class

--- TaskClasses/WorkPlace.cs
+++ TaskClasses/WorkPlace.cs
@@ -13,5 +13,26 @@ namespace TaskClasses
         public Adress Adress { get; set; }
         public int WorkersCount { get; set; }
 
+        public WorkPlace() 
+        { 
+            
+        }
+
+        public WorkPlace(int id, string companyName, Adress adress, int workersCount)
+        {
+            Id = id;
+            CompanyName = companyName;
+            Adress = adress;
+            WorkersCount = workersCount;
+        }
+
+        public WorkPlace(WorkPlace a)
+        {
+            Id = a.Id;
+            CompanyName = a.CompanyName;
+            Adress = a.Adress;
+            WorkersCount = a.WorkersCount;
+        }
+
     }
 }
